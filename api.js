ceb296afe2e25fde0c56660396ee7d4ef3fe9f2e
Antonio Carretero <aclbros12@yahoo.es>
2016-06-30 09:44:22 +0000

    11.44

--- api.js
+++ api.js
@@ -5,7 +5,7 @@ var router = express.Router();
 // main page
 router.get('/', function (req, res){
     res.send({
-        ipaddress: req.ip,
+        ipaddress: req.headers['x-forwarded-for'],
         language: req.get('Accept-Language'),
         software: req.get('user-agent').match(/\(.+\)/)[0].slice(1, -1)
     });
